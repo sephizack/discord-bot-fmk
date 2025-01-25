acc28b7a68022443124a2d5050d40af94a088f5d
Tony BISEGNA <tony.bisegna@amadeus.com>
2025-01-25 09:58:55 +0100

    Fix demo bot

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import DiscordBotApp from './discord/discord_bot_app.js'
 import Config from 'config';
-import DemoApp from './modules/demoUserBot.js'
+import { DemoUserBot } from './modules/demoUserBot.js'
 
 let myBotConf = Config.MyDiscordBot1
 let myDiscordApp = new DiscordBotApp()
-myDiscordApp.createUserBot("DemoBot", DemoApp.DemoUserBot, myBotConf.token, myBotConf.channel, {
+myDiscordApp.createUserBot("DemoBot", DemoUserBot, myBotConf.token, myBotConf.channel, {
     config: myBotConf.config
 });
