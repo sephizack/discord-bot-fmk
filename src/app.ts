3ab7e839f60909eeb9c18c2705295e91fe5f9c67
sephizack <bisegna.tony@gmail.com>
2025-02-01 08:15:02 +0100

    Improvements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import DiscordBotApp from './discord/discord_bot_app.js'
 import Config from 'config';
 import { DemoUserBot } from './modules/demoUserBot.js'
 
+
+
 let myBotConf = Config.MyDiscordBot1
 let myDiscordApp = new DiscordBotApp()
 myDiscordApp.createUserBot("DemoBot", DemoUserBot, myBotConf.token, myBotConf.channel, {
